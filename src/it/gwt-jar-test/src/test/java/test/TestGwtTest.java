2ae0bb62d21b134f206da7ede0730b0b64b570e0
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-02-02 13:14:00 +1100

    GWT Inherit walkingkooka-net-Net

--- src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
+++ src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
@@ -3,7 +3,9 @@ package test;
 import com.google.gwt.junit.client.GWTTestCase;
 import walkingkooka.store.Stores;
 
+@walkingkooka.j2cl.locale.LocaleAware
 public class TestGwtTest extends GWTTestCase {
+
     @Override
     public String getModuleName() {
         return "test.Test";
