28a93eb04284f191979d4f4ca91ecdee9e5bb613
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-06-10 21:10:56 +1000

    MultiValueStoreWatcher

---
+++ src/main/java/walkingkooka/store/MultiValueStoreWatcher.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.store;
+
+import java.util.Optional;
+
+/**
+ * A {@link StoreWatcher} for {@link MultiValueStore}
+ */
+public interface MultiValueStoreWatcher<K, V> extends StoreWatcher<V> {
+
+    /**
+     * Fired by {@link MultiValueStore#add(Object, Object)} when a new value is added.
+     */
+    void onValueAdded(final K id,
+                      final V value);
+
+    /**
+     * Fired by {@link MultiValueStore#add(Object, Object)} when an old value is removed.
+     */
+    void onValueRemoved(final K id,
+                        final V value);
+
+    // StoreWatcher.....................................................................................................
+
+    @Override
+    default void onValueChange(final Optional<V> oldValue,
+                               final Optional<V> newValue) {
+        throw new UnsupportedOperationException();
+    }
+}
